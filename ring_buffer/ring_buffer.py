d28ed852c327b25ec992d7e58d5f52e29cfcff98
Gabriel Samaniego <selfdev.gs@gmail.com>
2019-12-20 13:57:53 -0800

    buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -13,43 +13,47 @@ class RingBuffer:
 
     def append(self, item):
         # self.current = item
-        if self.storage.__len__() > self.capacity:
-            self.storage.add_to_head(item)
-        else:
-            self.storage.remove_from_tail()
-            self.storage.add_to_tail(item)
+        # keep adding to tail until max capacity
+        # if max capacity, replace head with item
 
-        # if full capacity, replace
+        if self.storage.__len__() > self.capacity:
+            print('max capacity')
+            self.storage.delete(self.storage.head)
+            self.storage.head = item
+        else:
+            print(item)
+            self.storage.add_to_tail(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
-        # num = 0
-        # while num < self.storage.__len__():
-        #     current_head = self.storage.head
-        #     list_buffer_contents.append(current_head.value)
-        #     self.storage.move_to_end(current_head)
-        #     num += 1
 
-        # for i in self.storage.__len__():
-        #     print(i)
-        #     if i is not None:
-        #         list_buffer_contents.append(i)
+        current = self.storage.head
+        # next_node = current.next
+
+        for _ in range(self.storage.__len__()):
+            # print('current next', current.next)
+            if current is not None:
+                # print('current -->', current)
+                list_buffer_contents.append(current.value)
+            current = current.next
 
         return list_buffer_contents
 
 
-buffer = RingBuffer(3)
+# buffer = RingBuffer(3)
 
-buffer.get()   # should return []
+# buffer.get()   # should return []
 
-buffer.append('a')
-buffer.append('b')
-buffer.append('c')
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+# buffer.append('d')
+# buffer.append('e')
 
-print(buffer.get())
+# print(buffer.get())
 
 
 # ----------------Stretch Goal-------------------
